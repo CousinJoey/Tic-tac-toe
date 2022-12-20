4343b9ccb3033cc2d3bbf89ee592b51657f41a17
CousinJoey <cousinjoeyllc@gmail.com>
2022-12-20 00:48:03 -0500

    added gameboard grid, some function foundation, and UI testing

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function empty() {
-    console.log("empty function")
+
+
+const gameBoardObject = (() => {
+    let gameBoard = [
+        "", "", "",
+        "", "", "",
+        "", "", "",
+    ]
+
+    return {gameBoard}
+
+})
+
+const displayController = (() => {
+    const tiles = document.querySelectorAll(".rows");
+
+    tiles.forEach(tile => {
+        tile.addEventListener("click", (e) => {
+            testFunc(e)
+        })
+    })
+    
+})
+
+const playerFactory = (player, marker) => {
+    return {player, marker};
+}
+
+const playerOne = playerFactory("player One", "X")
+const playerTwo = playerFactory("player Two", "O")
+
+displayController();
+
+function testFunc(e) {
+
+    const selectedTile = document.querySelector(`#${e.target.id}`)
+
+    const placedMarker = document.createElement("p")
+    placedMarker.textContent = playerOne.marker
+
+    selectedTile.appendChild(placedMarker)
 }
