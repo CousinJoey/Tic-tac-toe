d73af2c01376065477ee55731fdb1017634963ea
CousinJoey <cousinjoeyllc@gmail.com>
2022-12-21 21:01:46 -0500

    Added foundations to place a marker, with updating DOM

--- script.js
+++ script.js
@@ -7,20 +7,34 @@ const gameBoardObject = (() => {
         "", "", "",
     ]
 
-    return {gameBoard}
+   return {
+    gameBoard
+   };
 
-})
+})();
+
 
 const displayController = (() => {
-    const tiles = document.querySelectorAll(".rows");
-
-    tiles.forEach(tile => {
-        tile.addEventListener("click", (e) => {
-            testFunc(e)
-        })
-    })
     
-})
+    
+})();
+
+const gameBoardDisplay = (() => {
+
+    const divContainer = document.querySelector(".div-container")
+
+    for (let i = 0; i < (gameBoardObject.gameBoard).length; i++) {
+        const tile = document.createElement("div");
+        tile.id = `_${i}`
+        tile.classList = "rows"
+        divContainer.appendChild(tile)
+        tile.addEventListener("click", (e) => {
+            index = i
+            testFunc(e, index)
+        })
+    }
+
+})();
 
 const playerFactory = (player, marker) => {
     return {player, marker};
@@ -29,14 +43,20 @@ const playerFactory = (player, marker) => {
 const playerOne = playerFactory("player One", "X")
 const playerTwo = playerFactory("player Two", "O")
 
-displayController();
+function testFunc(e, index) {
+    let placedMarker = document.createElement("p");
+    placedMarker.innerText = "X";
+    console.log(index)
+    console.log(gameBoardObject.gameBoard[index]);
 
-function testFunc(e) {
+    if (gameBoardObject.gameBoard[index] == playerOne.marker) {
+        alert("already placed")
+    } else {
+        gameBoardObject.gameBoard.splice(index, 0, playerOne.marker);
+        gameBoardObject.gameBoard.splice((index + 1), 1);
+        (document.getElementById(e.target.id)).appendChild(placedMarker);
+    }
 
-    const selectedTile = document.querySelector(`#${e.target.id}`)
+    console.log(gameBoardObject.gameBoard);
 
-    const placedMarker = document.createElement("p")
-    placedMarker.textContent = playerOne.marker
-
-    selectedTile.appendChild(placedMarker)
 }
